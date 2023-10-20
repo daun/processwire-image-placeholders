f334a8d7b1376d87a8758b5d65452b7740241890
Philipp Daun <post@philippdaun.net>
2023-10-20 16:04:51 +0200

    Update module info

--- ImagePlaceholders.module.php
+++ ImagePlaceholders.module.php
@@ -10,22 +10,22 @@ wire('classLoader')->addNamespace('Daun', __DIR__ . '/lib');
 
 class ImagePlaceholders extends WireData implements Module
 {
-	static public function getModuleInfo()
+	public static function getModuleInfo()
 	{
 		return [
 			'title' => 'Image Placeholders',
 			'summary' => 'Generate low-quality image placeholders (LQIP) on upload',
-			'href' => 'http://modules.processwire.com/modules/image-placeholders/',
-			'version' => '0.1.0',
-			'author' => 'daun',
-			'singular' => true,
-			'autoload' => true,
+			'href' => 'https://processwire.com/talk/topic/29156-image-placeholders/',
+			'version' => '0.3.0',
+			'author' => 'Philipp Daun',
 			'icon' => 'picture-o',
 			'requires' => [
 				'PHP>=7.0',
 				'ProcessWire>=3.0.155',
 				'FieldtypeImage'
-			]
+			],
+			'singular' => true,
+			'autoload' => true
 		];
 	}
 
